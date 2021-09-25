b47b968afe83abba925432a23db88d7bc1d40928
Jesse LaSalle <jesselasalle@gmail.com>
2021-09-24 21:07:24 -0400

    Initial test config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "./controllers"
+import  "@hotwired/turbo-rails"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("packs/lunicode")
